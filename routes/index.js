74c031e24eed9412465266e25b19fdfadd80c23c
Aryan Faghihi <aryanfaghihi@gmail.com>
2020-11-20 22:24:04 +0100

    code clean up

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const metrics =require("./metrics")
 const router = express.Router();
-/* GET home page. */
 router.use("/metric",metrics)
 
 module.exports = router;
